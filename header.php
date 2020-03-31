b185285ec6089297e62e42300cb0bb89caf75f07
lilyackroyd <lilyackroyd@gmail.com>
2020-03-31 22:06:19 +0100

    Further tweak to search bar icon

--- header.php
+++ header.php
@@ -9,52 +9,52 @@
         <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"  crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" crossorigin="anonymous"></script>
     </head>
-    
-        
-        <header class="header">
- 
-  <input class="menu-btn" type="checkbox" id="menu-btn" />
-  <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
-  
-  <div class="logo">
-  <a href=""><img src="images/blog-logo-long.png" alt="logo" style="width:175px;border:0;"></a>
-  </div>
-  
-  <ul class="menu">
-     <div id="menu-left">
-                    <li><a class="active" href="#family">FAMILY</a></li>
-                    <li><a class="active" href="#search">FOOD</a></li>
-                    <li><a class="active" href="#search">KEEP FIT</a></li>
-                    <li> <a class="active" href="#search">CRAFT</a></li>
-</div>
- <div id="menu-right">
-     <li>
-         <form>
-  <input type="text" name="search" placeholder="">
-</form>
-     </li>
-                    <li><a id="log-in" class="active" href="#search"> Log in </a></li>
-                    <li><a class="active" href="#search">My Account</a></li>
-                
-  </div>
-</ul>
-      </header>      <body>
- 
-<script>
-        $("#inpt_search").on('focus', function () {
-	$(this).parent('label').addClass('active');
-});
 
-$("#inpt_search").on('blur', function () {
-	if($(this).val().length == 0)
-		$(this).parent('label').removeClass('active');
-});</script>   
 
-        
-        
-        
+    <header class="header">
+
+        <input class="menu-btn" type="checkbox" id="menu-btn" />
+        <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
+
+        <div class="logo">
+            <a href=""><img src="images/blog-logo-long.png" alt="logo" style="width:175px;border:0;"></a>
+        </div>
+
+        <ul class="menu">
+            <div id="menu-left">
+                <li><a class="active" href="#family">FAMILY</a></li>
+                <li><a class="active" href="#search">FOOD</a></li>
+                <li><a class="active" href="#search">KEEP FIT</a></li>
+                <li> <a class="active" href="#search">CRAFT</a></li>
+            </div>
+            <div id="menu-right">
+                <li>
+                    <form>
+                        <input type="text" name="search" placeholder="search...">
+                    </form>
+                </li>
+                <li><a id="log-in" class="active" href="#search"> Log in </a></li>
+                <li><a class="active" href="#search">My Account</a></li>
+
+            </div>
+        </ul>
+    </header>      <body>
+
+        <script>
+            $("#inpt_search").on('focus', function () {
+                $(this).parent('label').addClass('active');
+            });
+
+            $("#inpt_search").on('blur', function () {
+                if ($(this).val().length == 0)
+                    $(this).parent('label').removeClass('active');
+            });</script>   
+
+
+
+
     </body>
-        
+
 
 </html>
 
