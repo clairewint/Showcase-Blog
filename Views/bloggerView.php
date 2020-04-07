91b7b3c6ae9722c71981775f99de9181090ac060
lilyackroyd <lilyackroyd@gmail.com>
2020-04-07 16:19:47 +0100

    Added skeleton of blogger acc page

--- Views/bloggerView.php
+++ Views/bloggerView.php
@@ -1,12 +1,150 @@
-<?php
+<?php session_start();
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-session_start();
+//var_dump($_SESSION); //testing sessions are working and directing to correct page
+?>
 
-var_dump($_SESSION); //testing sessions are working and directing to correct page
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>The Awesome Isolation Blog</title>
+        <link rel=stylesheet href="CSS/admin-account.css">
+        <link rel=stylesheet href="CSS/site-wide.css">
+        <link href="https://fonts.googleapis.com/css2?family=Cabin&family=Open+Sans:wght@300&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <script src="http://code.jquery.com/jquery-1.8.3.min.js "  crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+    </head>
+    <body>
+
+        <!----page-container - This is the container that wraps the whole page including header and footer---->
+        <div id="page-container">
 
 
+
+
+            <!----Including the header / navigation menu------------------------------------------------->    
+            <?php
+            include 'header.php';
+            ?>
+
+
+
+
+            <!----content-wrap  - this is the container that holds just the content, minus the header & footer--->
+            <div id="content-wrap">
+
+
+
+
+                <!----intro-section  - if needed. This is where the page title and subheading go------------>        
+               
+                <div class="table-container" role="table" aria-label="">
+                    <div class="flex-table row" role="rowgroup">  
+                        <section class="intro-section-account">
+                        <div class="flex-row-intro" role="cell"> 
+                        <h1>Hello, Lily!</h1>  
+                        </div> 
+                        
+                        <div class="flex-row-intro" role="cell"> 
+                        <img class="account-img" src="images/SarahConnor.jpg"/>
+                        </div> 
+                        </section>
+                  </div>
+                </div>
+            
+
+
+
+
+                <div class="table-container" role="table" aria-label="">
+                    <div class="flex-table row" role="rowgroup">  
+
+                        
+                        <div class="flex-row" role="cell">
+                            <div class="card" style="width: 22rem;"><a class="acc-link" href="#">
+                                <div class="card-body">
+                                    <img class="acc-icon" src="images/plus2.png"style="width: 70px;height: 70px;"/>
+                                    <h2 class="card-title">Create a blog</h2>
+                              </div>
+                            </a>
+                        </div></div>
+
+                        
+                        
+                        
+                         <div class="flex-row" role="cell">
+                            <div class="card" style="width: 22rem;"><a class="acc-link" href="#" >
+                                <div class="card-body">
+                                    <img class="acc-icon" src="images/edit.png"style="width: 70px;height: 70px;"/>   
+                                    <h2 class="card-title">My blogs</h2>
+                                </div></a>
+                            </div>
+                        </div>
+
+                        
+                        
+                        
+                        <div class="flex-row" role="cell">
+                            <div class="card" style="width: 22rem;"><a class="acc-link" href="#" >
+                                <div class="card-body">
+                                      <img class="acc-icon"src="images/settings.png"style="width: 70px;height: 70px;"/> 
+                                    <h2 class="card-title">Account details</h2>
+                                </div></a>
+                            </div>
+                        </div>
+
+
+
+ </div>
+                   
+</div>
+
+
+
+                <!----end of content-wrap div---------------------------------------------------------------->
+            </div> 
+
+
+
+            <!----Including the footer------------------------------------------------------------------>   
+            <?php
+            include 'footer.php';
+            ?>   
+        
+
+        
+      <script>
+                $('.card')
+                        .on('mouseenter', function () {
+                            var div = $(this);
+                            div.stop(true, true).animate({
+                                margin: -10,
+                                width: "+=10",
+                                height: "+=10"
+                            }, 'fast');
+                        })
+                        .on('mouseleave', function () {
+                            var div = $(this);
+                            div.stop(true, true).animate({
+                                margin: 0,
+                                width: "-=10",
+                                height: "-=10"
+                            }, 'fast');
+                        })
+            </script>  
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    </body>
+
+</html>
